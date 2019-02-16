f9837523402e944e9efd672bcf631f5048dd28bd
Anders Gro Larsen <anders@uptime.dk>
2019-02-16 15:43:05 +0100

    Use json storage lib for prac

--- bot/storage/storage.js
+++ bot/storage/storage.js
@@ -1,6 +1,19 @@
+const path = require('path')
+const fs = require('fs')
+
 class Storage {
-    path(filename) {
-        return process.cwd() + '/storage/json/' + filename
+    path(filePath) {
+        return process.cwd() + '/storage/json/' + filePath
+    }
+
+    folderName(filePath) {
+        return path.dirname(filePath)
+    }
+
+    folderCheck(filePath) {
+        if (!fs.existsSync(this.folderName(filePath))) {
+            fs.mkdirSync(this.folderName(filePath))
+        }
     }
 }
 
