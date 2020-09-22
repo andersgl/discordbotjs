17b0bed86b0f9625213be39b761d63ebf70db1b5
Anders Gro Larsen <andersgla@gmail.com>
2020-09-22 20:06:06 +0200

    Update lan countdown

--- bot/lolz/lolz.js
+++ bot/lolz/lolz.js
@@ -21,7 +21,31 @@ class Lolz {
             return message.respondTTS(this.randomizr.random(99))
         }
         if (message.content.toLowerCase() == 'er det lan?') {
-            return message.respond('Nej, der er lang tid til lan')
+            const lanDate = moment('2020-11-13T00:00:00').utcOffset(1);
+            if (!lanDate) {
+                return message.respond('Nej, der er lang tid til lan');
+            }
+            const now = moment().utcOffset(1);
+            if (lanDate.unix() - now.unix() <= 0) {
+                return message.respond('JA! https://tenor.com/view/computer-fire-computer-on-fire-fire-computer-gif-12007576');
+            }
+
+            const duration = moment.duration(lanDate.diff(now));
+            const units = [
+                { key: 'months', single: 'måned', plural: 'måneder' },
+                { key: 'days', single: 'dag', plural: 'dage' },
+                { key: 'hours', single: 'time', plural: 'timer' },
+                { key: 'minutes', single: 'minut', plural: 'minutter' },
+                { key: 'seconds', single: 'sekund', plural: 'sekunder' },
+            ];
+            const unitDiffs = units.map(unit => {
+                const value = duration.get(unit.key);
+                if (!value) {
+                    return false;
+                }
+                return value + ' ' + (value > 1 ? unit.plural : unit.single);
+            }).filter(value => value !== false).join(', ');
+            return message.respond(`Nej, der er ${unitDiffs} til lan`);
         }
     }
 
